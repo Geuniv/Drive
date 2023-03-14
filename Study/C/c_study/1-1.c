c12a16b73fbd60fd210cbe5a8d2f4df03bc570e5
Geuni <116066819+Geuniv@users.noreply.github.com>
2023-03-14 17:49:56 +0900

    Add files via upload

---
+++ Study/C/c_study/1-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int result;
+	result = 100 - 50;
+	printf("%d", result);
+
+
+}
+
---
+++ Study/JAVA/myJavaPRJ/src/main/java/Quiz.java
@@ -0,0 +1,18 @@
+public class Quiz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+
+        for (i < j; i--){
+            for (j > i; i--){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
