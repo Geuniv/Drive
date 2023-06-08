fec9b64ec0861f6546a601ada8f7e165e9212cd3
Geuni <116066819+Geuniv@users.noreply.github.com>
2023-06-08 18:25:40 +0900

    2023.06.08 ( C언어 13장 예제 )
    2023.06.08 ( C언어 13장 예제 )

---
+++ Study/C/c_study/13-5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+	/* 구조체형을 선언함 */
+	struct student {
+	char name[10];
+	int kor;
+	int eng;
+	float avg;
+	}
+
+	/* 구조체형 배열 s[3]을 선언함 */
+	struct student s[3];
+
+	int i;
+
+	/* 첫 번째 학생의 정보를 대입함 */
+	strcpy(s[0], name, "Kim");
+	s[0].kor = 90;
+	s[0].eng = 80;
+	s[0].avg = (s[0].kor + s[0].eng) / 2.0f;
+	
+	/* 두 번째 학생의 정보를 대입함 */
+	strcpy(s[1], name, "Lee");
+	s[1].kor = 70;
+	s[1].eng = 60;
+	s[1].avg = (s[1].kor + s[1].eng) / 2.0f;
+	
+	/* 세 번째 학생의 정보를 대입함 */
+	strcpy(s[2], name, "Park");
+	s[2].kor = 50;
+	s[2].eng = 40;
+	s[2].avg = (s[2].kor + s[2].eng) / 2.0f;
+
+	/* 세 번 반복해서 배열의 내용을 출력함 */
+	printf("-- 구조체 배열 -- \n");
+	for (i=0; i<3; i++)
+	{
+		printf("학생 이름 ==> %s\n", s[i].name);
+		printf("국어 점수   ==> %d\n", s[i].kor);
+		printf("영어 점수   ==> %d\n", s[i].eng);
+		printf("평균 점수   ==> %5.1f\n", s[i].avg);
+	printf("\n");
+	}
+}
